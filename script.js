1b7e7058f9cc8498a456d6b5e73967eb27a60a2e
IrvingAcost34 <irving.acosta2026@jupa.superate.org.pa>
2026-06-23 09:15:54 -0500

    Add event listeners for navigation buttons

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+// BOTONES LOGIN Y SIGN UP
 
+const loginBtn = document.querySelector(".login-btn");
+const signupBtn = document.querySelector(".signup-btn");
+
+loginBtn.addEventListener("click", () => {
+    window.location.href = "Login.html";
+});
+
+signupBtn.addEventListener("click", () => {
+    window.location.href = "Login.html";
+});
+
+// BOTÓN START ASSESSMENT
+
+const startBtn = document.querySelector(".start-btn");
+
+startBtn.addEventListener("click", () => {
+    window.location.href = "Login.html";
+});
+
+// BOTÓN EXPLORE
+
+const exploreBtn = document.querySelector(".explore-btn");
+
+exploreBtn.addEventListener("click", () => {
+    alert("Learning Techniques section coming soon!");
+});
+
+// LOGO THINKING
+
+const brand = document.querySelector(".brand");
+
+brand.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
